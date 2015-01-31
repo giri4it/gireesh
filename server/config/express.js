0a9dbff050e699d7edf61c95dc19786746fbf2de
Gireesh Babu <gireesh.babu@corelogic.co.uk>
2015-02-01 00:04:36 +0530

    heroku failing

--- server/config/express.js
+++ server/config/express.js
@@ -29,8 +29,8 @@ module.exports = function(app) {
 
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + 'dist');
+    app.use(express.static(path.join(config.root, '/dist')));
+    app.set('appPath', config.root + '/dist');
     app.use(morgan('dev'));
   }
 

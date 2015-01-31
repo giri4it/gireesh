cec4e971256f2e830a9bf905b158a091df2710ca
Gireesh Babu <gireesh.babu@corelogic.co.uk>
2015-02-01 00:11:50 +0530

    heroku failing

--- server/config/express.js
+++ server/config/express.js
@@ -29,16 +29,16 @@ module.exports = function(app) {
 
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, '/dist')));
-    app.set('appPath', config.root + '/dist');
+    app.use(express.static(path.join(config.root,__dirname + '/dist')));
+    app.set('appPath', config.root + __dirname +'/dist');
     app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
     //app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, '/dist')));
-    app.set('appPath', '/dist');
+    app.use(express.static(path.join(config.root, __dirname +'/dist')));
+    app.set('appPath', __dirname +'/dist');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
